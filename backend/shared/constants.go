fe87fe7696ee710f63d67cc34824075e8d43f772
Immanuel Dsouza <dsouzad681@gmail.com>
2024-09-21 18:21:24 +0530

    secret-commit

---
+++ backend/shared/constants.go
@@ -0,0 +1,8 @@
+package shared
+
+const AUTH_TOKEN string = "authToken"
+const REFRESH_TOKEN string = "refreshToken"
+const USERS string = "users"
+const LIMITER string = "limiter"
+const X_CSRF_Token string = "X-CSRF-Token"
+const POSTMAN_TOKEN string = "Postman-Token"
